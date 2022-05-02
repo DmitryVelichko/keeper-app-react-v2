c2e3601753bdffcf7adea4d949e7e0a180c648f8
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-02 13:54:12 +0300

    new commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
     <footer>
-        Copyright ⓒ {new Date().getFullYear()}
+      <p>Copyright ⓒ {new Date().getFullYear()}</p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
