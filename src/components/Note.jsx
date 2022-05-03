6f5c3200a4d5fced2b9b5d0de34bb75f44738ad1
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-03 23:30:41 +0300

    new commit

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Note = () => {
   return (
     <div className='note'>
-        <h1>This is the title</h1>
+        <h1></h1>
         <p>This is the content</p>
     </div>
   )
