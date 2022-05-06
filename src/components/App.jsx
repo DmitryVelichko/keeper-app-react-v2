c9e80ca056b5ea1999ff617431f96d76b06edc8d
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-07 00:07:32 +0300

    new commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   function deleteNote(id) {
     setNotes(prevNotes => {
-      prevNotes.filter((noteItem, index) => {
+      return prevNotes.filter((noteItem, index) => {
         return index !== id;
       })
     })
