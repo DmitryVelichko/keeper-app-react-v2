68dace0744f9223042b2f87c49a30df1e89dc3e6
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-07 00:11:11 +0300

    new commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,9 +28,11 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem) => {
+      {notes.map((noteItem, index) => {
         return (
           <Note
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
