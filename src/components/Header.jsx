26f5bada4b072749ae5764bcd838eea2968e4adf
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-08 23:46:12 +0300

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import HighlightIcon from '@material-ui/icons/Highlight'
+import React from 'react';
+import HighlightIcon from '@material-ui/icons/Highlight';
 
 const Header = () => {
   return (
     <header>
-        <h1><HighlightIcon />Keeper</h1>
+      <h1>
+        <HighlightIcon />
+        Keeper
+      </h1>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
